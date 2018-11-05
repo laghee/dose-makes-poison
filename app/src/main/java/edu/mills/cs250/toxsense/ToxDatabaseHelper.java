87c19e15243608b5c799af7fae8a1e9f6b8d5713
laghee <katrua@gmail.com>
2018-11-05 16:28:42 +0100

    No issue - Fix build ridiculousness

--- app/src/main/java/edu/mills/cs250/toxsense/ToxDatabaseHelper.java
+++ app/src/main/java/edu/mills/cs250/toxsense/ToxDatabaseHelper.java
@@ -6,14 +6,14 @@ import android.util.Log;
 import com.readystatesoftware.sqliteasset.SQLiteAssetHelper;
 
 
-public class ToxDatabaseHelper extends SQLiteAssetHelper {
+class ToxDatabaseHelper extends SQLiteAssetHelper {
 
     private static final String DATABASE_NAME = "toxdata";
     private static final int DATABASE_VERSION = 1;
     private static final String TAG = "ToxDbHelper";
 
 
-    public ToxDatabaseHelper(Context context) {
+    ToxDatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
         Log.d(TAG, "DatabaseHelper... initialized: "
                 + context.getDatabasePath(DATABASE_NAME).getAbsolutePath());
