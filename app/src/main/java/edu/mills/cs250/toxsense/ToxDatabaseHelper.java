8368f42b40260794ce10643a10b32130bdc65a90
laghee <katrua@gmail.com>
2019-02-02 21:00:48 +0100

    Issue #18 - Change reference database to use Substances Added to Food gov data, some cleanup

--- app/src/main/java/edu/mills/cs250/toxsense/ToxDatabaseHelper.java
+++ app/src/main/java/edu/mills/cs250/toxsense/ToxDatabaseHelper.java
@@ -8,13 +8,14 @@ import com.readystatesoftware.sqliteasset.SQLiteAssetHelper;
 
 class ToxDatabaseHelper extends SQLiteAssetHelper {
 
-    private static final String DATABASE_NAME = "toxdata";
+    private static final String DATABASE_NAME = "toxdata.db";
     private static final int DATABASE_VERSION = 1;
     private static final String TAG = "ToxDbHelper";
 
 
     ToxDatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        setForcedUpgrade();
         Log.d(TAG, "DatabaseHelper... initialized: "
                 + context.getDatabasePath(DATABASE_NAME).getAbsolutePath());
     }
